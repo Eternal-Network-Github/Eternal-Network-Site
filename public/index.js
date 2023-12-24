6e2400f954b04c225d9e02924f45da8861062174
ChampClient <htesdev@gmail.com>
2023-12-24 11:17:22 -0500

    Proxy fix (Forgot to change one line of code :/)

--- public/index.js
+++ public/index.js
@@ -33,6 +33,6 @@ form.addEventListener("submit", async (event) => {
 
     const url = search(address.value, searchEngine.value);
     sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
-    location.href = "/proxy.html"
+    location.href = "./eternal-proxy.html"
     // location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
